c18c0f809e768b9354ef97160e6c2f5aafe05ed8
Juan <jpcerronex@gmail.com>
2022-01-17 17:35:55 -0300

    added tank movement

--- Player.gd
+++ Player.gd
@@ -1,20 +1,26 @@
 extends KinematicBody2D
 
-
-
 export (int) var speed = 100
-export (float) var rotation_speed = 1.5
+export (float) var rotation_speed = 2.0
 var velocity = Vector2()
 var rotation_dir = 0
-var directions = [0, PI/4, PI/2, PI/2, 3*PI/4]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func isRotationWithinDeltaForDirections(direction1, direction2, rotDelta):
+	var isWithinDeltaFirstDirection = (rotation > direction1 - rotDelta) && (rotation < direction1 + rotDelta)
+	if (isWithinDeltaFirstDirection):
+		rotation = direction1
+	var isWithinDeltaSecondDirection = (rotation > direction2 - rotDelta) && (rotation < direction2 + rotDelta)
+	if (isWithinDeltaSecondDirection):
+		rotation = direction2
+	return isWithinDeltaFirstDirection || isWithinDeltaSecondDirection
+
 func _physics_process(delta):
 	rotation_dir = 0
-	var rotDiff = rotation_speed * delta
+	var rotDelta = rotation_speed * delta
 	velocity = Vector2(0, 0)
 	#Only one rotation is counted
 	if (rotation > 2*PI):
@@ -23,13 +29,7 @@ func _physics_process(delta):
 		rotation = rotation + 2*PI
 
 	if (Input.is_action_pressed("move_up") && Input.is_action_pressed("move_right")) || Input.is_action_pressed("move_down") && Input.is_action_pressed("move_left"):
-		var isWithinOriginalClamp = (rotation > PI/4 - rotDiff) && (rotation < PI/4 + rotDiff)
-		if (isWithinOriginalClamp):
-			rotation = PI/4
-		var isWithinAlternateClamp = (rotation > 5*PI/4 - rotDiff) && (rotation < 5*PI/4 + rotDiff)
-		if (isWithinAlternateClamp):
-			rotation = 5*PI/4
-		if !isWithinOriginalClamp && !isWithinAlternateClamp:
+		if !isRotationWithinDeltaForDirections(PI/4, 5*PI/4, rotDelta):
 			if (rotation < PI/4) || (rotation > 7*PI/4):
 				rotation_dir = 1
 			elif (rotation < 3*PI/4):
@@ -38,15 +38,13 @@ func _physics_process(delta):
 				rotation_dir = 1
 			elif (rotation < 7*PI/4):
 				rotation_dir = -1
-				
+		else:
+			if Input.is_action_pressed("move_up"):
+				velocity = Vector2(1,-1)
+			if Input.is_action_pressed("move_down"):
+				velocity = Vector2(-1,1)
 	elif (Input.is_action_pressed("move_up") && Input.is_action_pressed("move_left")) || Input.is_action_pressed("move_down") && Input.is_action_pressed("move_right"):
-		var isWithinOriginalClamp = (rotation > 3*PI/4 - rotDiff) && (rotation < 3*PI/4 + rotDiff)
-		if (isWithinOriginalClamp):
-			rotation = 3*PI/4
-		var isWithinAlternateClamp = (rotation > 7*PI/4 - rotDiff) && (rotation < 7*PI/4 + rotDiff)
-		if (isWithinAlternateClamp):
-			rotation = 7*PI/4
-		if !isWithinOriginalClamp && !isWithinAlternateClamp:
+		if !isRotationWithinDeltaForDirections(3*PI/4, 7*PI/4, rotDelta):
 			if (rotation < PI/4) || (rotation > 7*PI/4):
 				rotation_dir = -1
 			elif (rotation < 3*PI/4):
@@ -55,15 +53,13 @@ func _physics_process(delta):
 				rotation_dir = -1
 			elif (rotation < 7*PI/4):
 				rotation_dir = 1
-
+		else:
+			if Input.is_action_pressed("move_up"):
+				velocity = Vector2(-1,-1)
+			if Input.is_action_pressed("move_down"):
+				velocity = Vector2(1,1)
 	elif Input.is_action_pressed("move_up") || Input.is_action_pressed("move_down"):
-		var isWithinOriginalClamp = (rotation > 0 - rotDiff) && (rotation < 0 + rotDiff)
-		if (isWithinOriginalClamp):
-			rotation = 0
-		var isWithinAlternateClamp = (rotation > PI - rotDiff) && (rotation < PI + rotDiff)
-		if (isWithinAlternateClamp):
-			rotation = PI
-		if !isWithinOriginalClamp && !isWithinAlternateClamp:
+		if !isRotationWithinDeltaForDirections(0, PI, rotDelta):
 			if (rotation < PI/2):
 				rotation_dir = -1
 			elif (rotation < PI):
@@ -72,14 +68,13 @@ func _physics_process(delta):
 				rotation_dir = -1
 			elif (rotation < 2*PI):
 				rotation_dir = 1
+		else:
+			if Input.is_action_pressed("move_up"):
+				velocity = Vector2(0,-1)
+			if Input.is_action_pressed("move_down"):
+				velocity = Vector2(0,1)
 	elif Input.is_action_pressed("move_left") || Input.is_action_pressed("move_right"):
-		var isWithinOriginalClamp = (rotation > PI/2 - rotDiff) && (rotation < PI/2 + rotDiff)
-		if (isWithinOriginalClamp):
-			rotation = PI/2
-		var isWithinAlternateClamp = (rotation > 3*PI/2 - rotDiff) && (rotation < 3*PI/2 + rotDiff)
-		if (isWithinAlternateClamp):
-			rotation = 3*PI/2
-		if !isWithinOriginalClamp && !isWithinAlternateClamp:
+		if !isRotationWithinDeltaForDirections(PI/2, 3*PI/2, rotDelta):
 			if (rotation < PI/2):
 				rotation_dir = 1
 			elif (rotation < PI):
@@ -88,9 +83,14 @@ func _physics_process(delta):
 				rotation_dir = 1
 			elif (rotation < 2*PI):
 				rotation_dir = -1
+		else:
+			if Input.is_action_pressed("move_right"):
+				velocity = Vector2(1,0)
+			if Input.is_action_pressed("move_left"):
+				velocity = Vector2(-1,0)
 
 	#print_debug(rotation)
-	rotation += rotation_dir * rotDiff
+	rotation += rotation_dir * rotDelta
 	velocity = velocity.normalized() * speed
 	velocity = move_and_slide(velocity)
 
