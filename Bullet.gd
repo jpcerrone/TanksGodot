b13927fa36c7d07dfd68f7170df4a6bb5127b0a8
Juan <jpcerronex@gmail.com>
2022-02-06 12:54:37 -0300

    added bullet bounce and outer walls

--- Bullet.gd
+++ Bullet.gd
@@ -1,7 +1,9 @@
 extends KinematicBody2D
 
 
-var speed = 250.0
+const speed = 250.0
+const maxRebounds = 2
+var currentRebounds
 var velocity = Vector2()
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,8 +12,13 @@ func _ready():
 func setup(initialPosition: Vector2, initialVelocity: Vector2):
 	position = initialPosition
 	self.velocity = initialVelocity
+	currentRebounds = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	move_and_collide(velocity*delta*speed)
-	pass
+	var collision = move_and_collide(velocity*delta*speed)
+	if (collision):
+		velocity = velocity.bounce(collision.normal)
+		currentRebounds += 1;
+		if (currentRebounds > maxRebounds):
+			queue_free()
