252c39f04ca36291b632e78617ac339564e82b88
Juan <jpcerronex@gmail.com>
2022-03-06 14:08:22 -0300

    added grey tank + background

--- scenes/GreyTank.gd
+++ scenes/GreyTank.gd
@@ -1,27 +1,36 @@
 extends "res://scripts/Tank.gd"
 
 var rotationDirection = 1
-var rng = RandomNumberGenerator.new()
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var rng
+var okToShoot = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	rng = RandomNumberGenerator.new()
 
 func _physics_process(delta):
 	$Cannon.rotation += delta * rotationDirection
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func _on_Timer_timeout():
-	shoot()
-
+	if(okToShoot):
+		var cannonTipPos = position + Vector2(50, 0).rotated($Cannon.rotation)
+		var spaceState = get_world_2d().direct_space_state
+		#Replace 1000 with bounds for resolution
+		var result = spaceState.intersect_ray(cannonTipPos, cannonTipPos + Vector2(1,0).rotated($Cannon.rotation)*1000)
+		if (result):
+			if(result.collider.is_in_group('player')):
+				shoot()
+				okToShoot = false
+			if(result.collider.is_in_group('walls')):
+				var dirVector = Vector2(1,0).rotated($Cannon.rotation)
+				var newResult = spaceState.intersect_ray(result.position, result.position + dirVector.bounce(result.normal)*1000)
+				if(newResult && newResult.collider.is_in_group('player')):
+					shoot()
+					okToShoot = false
 func _on_ChangeDirectionTimer_timeout():
 	rotationDirection = -rotationDirection
 	$ChangeDirectionTimer.wait_time = rng.randf_range(0, 5.0)
+
+func _on_ShootingTimer_timeout():
+	if (!okToShoot):
+		okToShoot = true
+	$ShootingTimer.wait_time = rng.randf_range(0, 5.0)
