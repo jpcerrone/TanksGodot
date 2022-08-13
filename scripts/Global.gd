05e32cc9ffb1376707aff270d8fc3cd6e9862701
Juan <jpcerronex@gmail.com>
2022-08-13 11:14:14 -0300

    can now quit from paused game

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -5,3 +5,10 @@ const MAX_INT = 9223372036854775807
 
 # Global variables
 var p1Position: Vector2
+
+func _ready():
+	pause_mode = Node.PAUSE_MODE_PROCESS
+
+func _process(_delta):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
